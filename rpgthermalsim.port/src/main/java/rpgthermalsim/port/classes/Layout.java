a5cbb8f35c7625ae000449067485a14d1e67767b
David Baselga-Masia <dabama1@upv.es>
2019-04-18 17:01:16 +0200

    Ported the typedef structure from C++ to Java

---
+++ rpgthermalsim.port/src/main/java/rpgthermalsim/port/classes/Layout.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package rpgthermalsim.port.classes;
+
+import java.util.HashMap;
+
+/**
+ * @author dabama1
+ * typedef std::map<std::string, room*> layout;
+ */
+public class Layout extends HashMap<String, Room>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;}
