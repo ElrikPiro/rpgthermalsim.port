8f06d8b6c2e64cc1a5f81a9094f8528e3afe4326
David Baselga <baselga.david@gmail.com>
2019-04-24 11:06:54 +0200

    Parsed build command Parsed command set parsed link commands parsed the list command Parsed the ignite command Parsed deflagrate Parsed block command finished parsing the _command method

--- rpgthermalsim.port/src/main/java/rpgthermalsim/port/classes/Building.java
+++ rpgthermalsim.port/src/main/java/rpgthermalsim/port/classes/Building.java
@@ -27,7 +27,7 @@ public class Building {
 			while(bf.ready()) {
 				line = bf.readLine();
 				if(line.length()==0) line = "#";
-				_command(line);//TODO: this throws exception if failed
+				_command(line);
 				iterations++;
 			}
 			bf.close();
@@ -47,9 +47,17 @@ public class Building {
 		return;
 	}
 
-	private void _command(String line) throws BuildingException{
+	private void _command(String line) throws BuildingException, IOException{
 		String command;
 		String[] args;
+		String ID;
+		int w,h;
+		int x,y;
+		Integer flame = new Integer(0);
+		Integer ignition = new Integer(0);
+		Integer temperature  = new Integer(0);
+		
+		
 		args = line.split(" ");
 		command = args[0];
 		if(command.length()==0) {
@@ -75,277 +83,235 @@ public class Building {
 					iterate(Integer.parseInt(args[1]));
 				}else iterate();
 				refresh(ref);
-		}
-		/*
-		 * 
-		}else if(command=="iterate"){
-			bool a = (bool) std::getline(args,command,' ');
-			if(a&&std::atoi(command.c_str())>1) iterate(std::atoi(command.c_str()));
-			else iterate();
-			refresh(ref);
-			return 0;
-		}else if(command=="build"){//void newRoom(std::string ID,int w,int h,std::string desc)
-			std::string ID,desc;
-			int w,h;
-			if(std::getline(args,command,' ')){
-				ID = command;
-				if( this->buildingLayout.find(command) != this->buildingLayout.end() ) return 3;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				w = std::atoi(command.c_str());
-				if( w<1 ) return 2;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				h = std::atoi(command.c_str());
-				if( h<1 ) return 2;
-			}else return 1;
-
-			if(std::getline(args,desc)){
-
-			}else desc = "Room without description";
-
-			newRoom(ID,w,h,desc);
-			ref[ID] = this->buildingLayout[ID];
-			builds.push_back(input);
-			return 0;
-		}else if(command=="set"){
-			std::string ID;
-			int w,h;
-			int flame=0,ignition=0,temperature=0;
-
-			if(std::getline(args,command,' ')){
-				ID = command;
-				if( this->buildingLayout.find(command) == this->buildingLayout.end() ) return 4;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				w = std::atoi(command.c_str())-1;
-				if( w<0 ) return 2;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				h = std::atoi(command.c_str())-1;
-				if( h<0 ) return 2;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				flame = std::atoi(command.c_str());
-				if( flame<0 || flame>1 ) return 2;
-			}
-
-			if(std::getline(args,command,' ')){
-				ignition = std::atoi(command.c_str());
-				if( flame > 0 && ignition > 0 ) return 2;
-			}
-
-			if(std::getline(args,command,' ')){
-				temperature = std::atoi(command.c_str());
-				if( temperature < 0 ) return 2;
-			}
-
-			setCell(ID,w,h,flame,ignition,temperature);
-			return 0;
-		}else if(command=="exit") return -1;
-		else if(command=="link"){
-			std::string ID1,ID2;
-			int w1,h1,w2,h2;
-
-			if(std::getline(args,command,' ')){
-				ID1 = command;
-				if( this->buildingLayout.find(command) == this->buildingLayout.end() ) return 3;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				w1 = std::atoi(command.c_str())-1;
-				if( w1<0 ) return 2;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				h1 = std::atoi(command.c_str())-1;
-				if( h1<0 ) return 2;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				ID2 = command;
-				if( this->buildingLayout.find(command) == this->buildingLayout.end() ) return 3;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				w2 = std::atoi(command.c_str())-1;
-				if( w2<0 ) return 2;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				h2 = std::atoi(command.c_str())-1;
-				if( h2<0 ) return 2;
-			}else return 1;
-
-			linkCells(ID1,w1,h1,ID2,w2,h2);
-			links.push_back(input);
-			return 0;
-		}else if(command=="list"){
-			for(auto it = this->buildingLayout.begin();it!=this->buildingLayout.end();it++){
+				return;
+			case "build":
+				String desc;
+				if(args.length<4) throw new BuildingException("Build command requires at least 3 parameters.");
+				ID = args[1];
+				if(this.buildingLayout.containsKey(ID)) throw new BuildingException("Building "+ID+" already exists.");
+				w = Integer.parseInt(args[2]);
+				h = Integer.parseInt(args[3]);
+				if(w < 1 || h < 1) throw new BuildingException("witdh and height must be non-zero positive numbers.");
+				if(args.length>4) {
+					desc = "";
+					for(int i = 4; i<args.length;i++)
+						desc = desc+" "+args[i];
+					desc = desc.trim();
+				}
+				else desc = "Room without description";
+				
+				newRoom(ID,w,h,desc);
+				
+				ref.replace(ID, this.buildingLayout.get(ID));
+				builds.add(line);
+				return;
+			case "set":
+				Integer[] values = {flame,ignition,temperature};
+				
+				if(args.length<4) throw new BuildingException("set command requires at least 3 parameters.");
+				
+				ID = args[1];
+				if(!this.buildingLayout.containsKey(ID)) throw new BuildingException("Building "+ID+" does not exist.");
+				
+				w = Integer.parseInt(args[2]);
+				h = Integer.parseInt(args[3]);
+				if(w < 0 || h < 0) throw new BuildingException("pos x and y must be zero or positive numbers.");
+				
+				for(int i = 4;i<args.length;i++) {
+					if(i>6) break;
+					values[i-4] = new Integer(args[i]);
+					if(i-4 == 0 && values[i-4].intValue()<0 || values[i-4].intValue()>1) throw new BuildingException("flame value must be 1 or 0.");
+					if(i-5 == 0 && values[i-4].intValue()>0 && values[i-5].intValue()>0) throw new BuildingException("flame and ignition cant be positive at the same time.");
+					if(i-6 == 0 && values[i-6].intValue()<0) throw new BuildingException("temperature cannot be negative.");
+				}
+				
+				setCell(ID,w,h,values[0],values[1],values[2]);
+				return;
+			case "exit":
+				System.exit(0);
+			case "link":
+				String ID1,ID2;
+				int w1,h1,w2,h2;
+				
+				if(args.length<7) throw new BuildingException("link command requires at least 6 parameters.");
+				
+				ID1 = args[1];
+				ID2 = args[4];
+				if(!this.buildingLayout.containsKey(ID1)) throw new BuildingException("Building "+ID1+" does not exist.");
+				if(!this.buildingLayout.containsKey(ID2)) throw new BuildingException("Building "+ID2+" does not exist.");
+				
+				w1 = Integer.parseInt(args[2]);
+				h1 = Integer.parseInt(args[3]);
+				w2 = Integer.parseInt(args[5]);
+				h2 = Integer.parseInt(args[6]);
+				if(w1 < 0 || h1 < 0 || w2 < 0 || h2 < 0) throw new BuildingException("pos x and y must be zero or positive numbers.");
+				
+				linkCells(ID1,w1,h1,ID2,w2,h2);
+				
+				links.add(line);
+				return;
+			case "list":
 				listRooms();
-				return -2;
-			}
-		}else if(command=="ignite"){
-			std::string ID1;
-			int x,y;
-
-			if(std::getline(args,command,' ')){
-				ID1 = command;
-				if( this->buildingLayout.find(command) == this->buildingLayout.end() ) return 3;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				x = std::atoi(command.c_str())-1;
-				if( x<0 ) return 2;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				y = std::atoi(command.c_str())-1;
-				if( y<0 ) return 2;
-			}else return 1;
-
-			ignite(ID1,x,y);
-			return 0;
-		}else if(command=="deflagrate"){
-			std::string ID1;
-			int x,y,r;
-				if(std::getline(args,command,' ')){
-				ID1 = command;
-					if( this->buildingLayout.find(command) == this->buildingLayout.end() ) return 3;
-				}else return 1;
-
-				if(std::getline(args,command,' ')){
-					x = std::atoi(command.c_str())-1;
-					if( x<0 ) return 2;
-				}else return 1;
-
-				if(std::getline(args,command,' ')){
-					y = std::atoi(command.c_str())-1;
-					if( y<0 ) return 2;
-				}else return 1;
-
-				if(std::getline(args,command,' ')){
-					r = std::atoi(command.c_str());
-					if( r<1 ) return 2;
-				}else r=1;
-
-				deflagrate(ID1,x,y,r);
-				return 0;
-		}else if(command=="block"){
-			std::string ID1;
-			int x,y;
-
-			if(std::getline(args,command,' ')){
-				ID1 = command;
-				if( this->buildingLayout.find(command) == this->buildingLayout.end() ) return 3;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				x = std::atoi(command.c_str())-1;
-				if( x<0 ) return 2;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				y = std::atoi(command.c_str())-1;
-				if( y<0 ) return 2;
-			}else return 1;
-
-			block(ID1,x,y);
-			puts.push_back(input);
-			return 0;
-		}else if(command=="unblock"){
-			std::string ID1;
-			int x,y;
-
-			if(std::getline(args,command,' ')){
-				ID1 = command;
-				if( this->buildingLayout.find(command) == this->buildingLayout.end() ) return 3;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				x = std::atoi(command.c_str())-1;
-				if( x<0 ) return 2;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				y = std::atoi(command.c_str())-1;
-				if( y<0 ) return 2;
-			}else return 1;
-
-			unblock(ID1,x,y);
-			puts.push_back(input);
-			return 0;
-		}else if(command=="put"){
-			std::string ID1;
-			int x,y,ignition;
-
-			if(std::getline(args,command,' ')){
-				ID1 = command;
-				if( this->buildingLayout.find(command) == this->buildingLayout.end() ) return 3;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				x = std::atoi(command.c_str())-1;
-				if( x<0 ) return 2;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				y = std::atoi(command.c_str())-1;
-				if( y<0 ) return 2;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				ignition = std::atoi(command.c_str());
-				if( ignition<0 ) return 2;
-			}else return 1;
-
-			setCell(ID1,x,y,0,ignition,0);
-			puts.push_back(input);
-			return 0;
-		}else if(command=="clear"){
-			std::string ID1;
-			int x,y;
-
-			if(std::getline(args,command,' ')){
-				ID1 = command;
-				if( this->buildingLayout.find(command) == this->buildingLayout.end() ) return 3;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				x = std::atoi(command.c_str())-1;
-				if( x<0 ) return 2;
-			}else return 1;
-
-			if(std::getline(args,command,' ')){
-				y = std::atoi(command.c_str())-1;
-				if( y<0 ) return 2;
-			}else return 1;
-
-			setCell(ID1,x,y,0,0,0);
-			puts.push_back(input);
-			return 0;
-		}else if(command=="save"){
-			std::string filename;
-			if(std::getline(args,command,' ')){
-				return save(command);
-			}else return save();
-		}else if(command=="load"){
-			std::string filename;
-			if(std::getline(args,command,' ')){
-				return load(command);
-			}else return load();
-		}else if(command=="reset"){
-			reset();
-			return 0;
+				return;
+			case "ignite":
+				
+				if(args.length<4) throw new BuildingException("ignite command requires at least 4 parameters.");
+				
+				ID = args[1];
+				if(!this.buildingLayout.containsKey(ID)) throw new BuildingException("Building "+ID+" does not exist.");
+				
+				x = Integer.parseInt(args[2]); y = Integer.parseInt(args[3]);
+				if(x < 0 || y < 0) throw new BuildingException("pos x and y must be zero or positive numbers.");
+				
+				ignite(ID,x,y);
+				return;
+			case "deflagrate":
+				int r = 1;
+				
+				if(args.length<4) throw new BuildingException("deflagrate command requires at least 4 parameters.");
+				
+				ID = args[1];
+				if(!this.buildingLayout.containsKey(ID)) throw new BuildingException("Building "+ID+" does not exist.");
+				
+				x = Integer.parseInt(args[2]); y = Integer.parseInt(args[3]);
+				if(x < 0 || y < 0) throw new BuildingException("pos x and y must be zero or positive numbers.");
+				
+				if(args.length>4) {
+					r = Integer.parseInt(args[4]);
+					if( r < 1 ) throw new BuildingException("radius must be a non-zero positive number.");
+				}
+				
+				deflagrate(ID,x,y,r);
+				return;
+			case "block":
+				if(args.length<4) throw new BuildingException("block command requires at least 4 parameters.");
+				
+				ID = args[1];
+				if(!this.buildingLayout.containsKey(ID)) throw new BuildingException("Building "+ID+" does not exist.");
+				
+				x = Integer.parseInt(args[2]); y = Integer.parseInt(args[3]);
+				if(x < 0 || y < 0) throw new BuildingException("pos x and y must be zero or positive numbers.");
+				
+				block(ID,x,y);
+				puts.add(line);
+				return;
+			case "unblock":
+				if(args.length<4) throw new BuildingException("unblock command requires at least 4 parameters.");
+				
+				ID = args[1];
+				if(!this.buildingLayout.containsKey(ID)) throw new BuildingException("Building "+ID+" does not exist.");
+				
+				x = Integer.parseInt(args[2]); y = Integer.parseInt(args[3]);
+				if(x < 0 || y < 0) throw new BuildingException("pos x and y must be zero or positive numbers.");
+				
+				unblock(ID,x,y);
+				puts.add(line);
+				return;
+			case "put":
+				if(args.length<5) throw new BuildingException("put command requires at least 4 parameters.");
+				
+				ID = args[1];
+				if(!this.buildingLayout.containsKey(ID)) throw new BuildingException("Building "+ID+" does not exist.");
+				
+				x = Integer.parseInt(args[2]); y = Integer.parseInt(args[3]);
+				if(x < 0 || y < 0) throw new BuildingException("pos x and y must be zero or positive numbers.");
+				
+				ignition = new Integer(Integer.parseInt(args[4]));
+				if(ignition < 0) throw new BuildingException("ignition value must be zero or positive numbers.");
+				
+				setCell(ID,x,y,0,ignition.intValue(),0);
+				puts.add(line);
+				return;
+			case "clear":
+				if(args.length<4) throw new BuildingException("clear command requires at least 4 parameters.");
+				
+				ID = args[1];
+				if(!this.buildingLayout.containsKey(ID)) throw new BuildingException("Building "+ID+" does not exist.");
+				
+				x = Integer.parseInt(args[2]); y = Integer.parseInt(args[3]);
+				if(x < 0 || y < 0) throw new BuildingException("pos x and y must be zero or positive numbers.");
+				
+				setCell(ID,x,y,0,0,0);
+				puts.add(line);
+				return;
+			case "save":
+				if(args.length>1) save(args[1]);
+				else save();
+				return;
+			case "load":
+				if(args.length>1) load(args[1]);
+				else load();
+				return;
+			case "reset":
+				reset();
+				return;
+			default:
+				throw new BuildingException("Command "+command+" not supported.");
 		}
+	}
 
-		return 1;
-		 */
+	private void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void load() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void load(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void save() throws IOException{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void save(String string) throws IOException{
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void unblock(String iD, int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void block(String iD, int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void deflagrate(String iD, int x, int y, int r) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void ignite(String iD, int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void listRooms() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void linkCells(String iD1, int w1, int h1, String iD2, int w2, int h2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setCell(String iD, int w, int h, Integer integer, Integer integer2, Integer integer3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void newRoom(String iD, int w, int h, String desc) {
+		// TODO Auto-generated method stub
 		
 	}
 
