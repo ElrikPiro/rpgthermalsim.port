50cfac5a2a2dafce5d23ed02251e13361c6ac2e3
David Baselga <baselga.david@gmail.com>
2019-04-25 11:24:58 +0200

    Populated the Cell methods

--- rpgthermalsim.port/src/main/java/rpgthermalsim/port/classes/Cell.java
+++ rpgthermalsim.port/src/main/java/rpgthermalsim/port/classes/Cell.java
@@ -4,6 +4,10 @@ import java.util.Set;
 
 public class Cell {
 
+	public Cell(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public void setStatus(int intValue, int intValue2, int intValue3) {
 		// TODO Auto-generated method stub
 		
@@ -35,6 +39,36 @@ public class Cell {
 	}
 
 	public void setReachable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void spread() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void commitStatus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void checkFlashpoint() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void dissipateHeat() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public String toString() {
+		return null;
+		// TODO
+	}
+
+	public void addNeightbour(Cell cell) {
 		// TODO Auto-generated method stub
 		
 	}
