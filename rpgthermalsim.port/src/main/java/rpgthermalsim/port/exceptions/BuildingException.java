00f1ec9fc972895f142a4bcb0e0f3e181e448da7
David Baselga <baselga.david@gmail.com>
2019-05-06 11:00:02 +0200

    Removed unnecesary overrides.

--- rpgthermalsim.port/src/main/java/rpgthermalsim/port/exceptions/BuildingException.java
+++ rpgthermalsim.port/src/main/java/rpgthermalsim/port/exceptions/BuildingException.java
@@ -2,27 +2,8 @@ package rpgthermalsim.port.exceptions;
 
 public class BuildingException extends Exception {
 
-	public BuildingException() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public BuildingException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
-	}
-
-	public BuildingException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
-
-	public BuildingException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
-
-	public BuildingException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
 		// TODO Auto-generated constructor stub
 	}
 
