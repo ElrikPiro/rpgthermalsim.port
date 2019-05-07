66ad45760558c74a3825f0e209c9eb8c6188e1f0
David Baselga <baselga.david@gmail.com>
2019-05-07 10:30:49 +0200

    created fynction to add and remove sink cells at a given location

--- rpgthermalsim.port/src/main/java/rpgthermalsim/port/classes/Building.java
+++ rpgthermalsim.port/src/main/java/rpgthermalsim/port/classes/Building.java
@@ -78,6 +78,8 @@ public class Building implements Digestable{
 		Integer flame = new Integer(0);
 		Integer ignition = new Integer(0);
 		Integer temperature  = new Integer(0);
+		Iterator<Cell> it;
+		Cell c;
 		
 		
 		args = line.split(" ");
@@ -269,6 +271,38 @@ public class Building implements Digestable{
 				return;
 			case "reset":
 				reset();
+				return;
+			case "sink":
+				if(args.length<5) throw new BuildingException("sink command requires at least 5 parameters.");
+				
+				ID = args[1];
+				if(!this.buildingLayout.containsKey(ID)) throw new BuildingException("Building "+ID+" does not exist.");
+				
+				x = Integer.parseInt(args[2]); y = Integer.parseInt(args[3]);
+				if(x < 0 || y < 0) throw new BuildingException("pos x and y must be zero or positive numbers.");
+				
+				this.buildingLayout.get(ID).getCellXY(x, y).linkCells(new FixedTempCell(Integer.parseInt(args[4])));
+				
+				puts.add(line);
+				return;
+			case "unsink":
+				if(args.length<4) throw new BuildingException("unsink command requires at least 4 parameters.");
+				
+				ID = args[1];
+				if(!this.buildingLayout.containsKey(ID)) throw new BuildingException("Building "+ID+" does not exist.");
+				
+				x = Integer.parseInt(args[2]); y = Integer.parseInt(args[3]);
+				if(x < 0 || y < 0) throw new BuildingException("pos x and y must be zero or positive numbers.");
+				
+				it = this.buildingLayout.get(ID).getCellXY(x, y).getNeightbourhood().iterator();
+				
+				while(it.hasNext()) {
+					c = it.next();
+					if(c.getClass() == FixedTempCell.class) {
+						this.buildingLayout.get(ID).getCellXY(x, y).getNeightbourhood().remove(c);
+					}
+				}
+				puts.add(line);
 				return;
 			default:
 				throw new BuildingException("Command "+command+" not supported.");
